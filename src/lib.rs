ee208c9674abf94a957ac2cfb81f63ca8ab60f84
Andrew Barchuk <andrew@raindev.io>
2016-06-12 22:12:18 +0300

    Fix empty Vec and empty string cases of join_split
    Add Quickcheck based test and fix caught problems

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,14 @@
 pub mod join_split {
-    pub fn join(ss: &Vec<&str>) -> String {
-        ss.iter()
-            .map(escape)
+    pub fn join(ss: &Vec<String>) -> String {
+        if ss.is_empty() {
+            return String::new()
+        }
+        let mut string = ss.iter()
+            .map(|s| s.replace('`', "``"))
             .collect::<Vec<String>>()
-            .join("`$")
-    }
-
-    fn escape(s: &&str) -> String {
-        s.replace('`', "``")
+            .join("`$");
+        string.push_str("`$");
+        string
     }
 
     pub fn split(s: &str) -> Vec<String> {
@@ -29,21 +30,50 @@ pub mod join_split {
                 next_str.push(chr);
             }
         }
-        if !next_str.is_empty() {
-            res.push(next_str);
-        }
         res
+    }
+
+}
+
+#[cfg(test)]
+mod test_join_split {
+    use join_split::*;
+    extern crate quickcheck;
+    use self::quickcheck::quickcheck;
+
+    fn to_str_vec(v: Vec<&str>) -> Vec<String> {
+        v.iter().map(|s| str::to_owned(*s)).collect()
     }
 
     #[test]
     fn join_split() {
-        let ss = vec!["hi", "there", "people"];
+        let ss = to_str_vec(vec!["hi", "there", "people"]);
         assert_eq!(ss, split(&join(&ss)));
     }
 
     #[test]
     fn join_split_if_separator_present() {
-        let ss = vec!["h`i", "the``re", "people"];
+        let ss = to_str_vec(vec!["h`i", "the``re", "people"]);
         assert_eq!(ss, split(&join(&ss)));
+    }
+
+    #[test]
+    fn join_split_empty_string() {
+        let ss = to_str_vec(vec![""]);
+        assert_eq!(ss, split(&join(&ss)));
+    }
+
+    #[test]
+    fn join_split_empty_vec() {
+        let ss = to_str_vec(vec![]);
+        assert_eq!(ss, split(&join(&ss)));
+    }
+
+    #[test]
+    fn test_exhaustively() {
+        fn prop(ss: Vec<String>) -> bool {
+            ss == split(&join(&ss))
+        }
+        quickcheck(prop as fn(Vec<String>) -> bool);
     }
 }
