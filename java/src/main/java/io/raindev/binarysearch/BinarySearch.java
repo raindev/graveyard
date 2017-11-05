020e01c2674fdfd90a572802126c8b4f0048895a
Andrew Barchuk <andrew@raindev.io>
2017-11-05 10:53:58 +0100

    Implement recursive binary search in Java

---
+++ java/src/main/java/io/raindev/binarysearch/BinarySearch.java
@@ -0,0 +1,42 @@
+package io.raindev.binarysearch;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public class BinarySearch {
+
+    static <T extends Comparable<T>> Optional<Integer> binarySearch(
+            T[] sortedArray, T element) {
+        return binarySearch(sortedArray, element, 0, sortedArray.length - 1);
+    }
+
+    private static <T extends Comparable<T>> Optional<Integer> binarySearch(
+                    T[] arr, T elem, int start, int end) {
+        if (start > end) {
+            return Optional.empty();
+        }
+        int middle = (start + end) / 2;
+        T middleElem = arr[middle];
+        if (Objects.equals(middleElem, elem)) {
+            return Optional.of(middle);
+        // greater than
+        } else if (compare(elem, middleElem) > 0) {
+            return binarySearch(arr, elem, middle + 1, end);
+        } else {
+            return binarySearch(arr, elem, start, middle - 1);
+        }
+    }
+
+    // null first
+    private static <T extends Comparable<T>> int compare(T x, T y) {
+        if (x == null) {
+            if (y == null) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+        return x.compareTo(y);
+    }
+
+}
---
+++ java/src/test/java/io/raindev/binarysearch/BinarySearchTest.java
@@ -0,0 +1,41 @@
+package io.raindev.binarysearch;
+
+import java.util.Optional;
+import org.junit.Test;
+import static io.raindev.binarysearch.BinarySearch.binarySearch;
+import static org.junit.Assert.assertEquals;
+
+public class BinarySearchTest {
+    Integer[] array =  new Integer[]{null, 3, 4, 6,7, 8, 11};
+
+    @Test public void findExisting() {
+        assertEquals(Optional.of(2),
+                binarySearch(array, 4));
+    }
+
+    @Test public void searchForMissing() {
+        assertEquals(Optional.empty(),
+                binarySearch(array, 5));
+    }
+
+    @Test public void searchForNull() {
+        assertEquals(Optional.of(0),
+                binarySearch(array, null));
+    }
+
+    @Test public void searchInEmpty() {
+        assertEquals(Optional.empty(),
+                binarySearch(new Integer[]{}, 9));
+    }
+
+    @Test public void findInSingletonArray() {
+        assertEquals(Optional.of(0),
+                binarySearch(new Integer[]{9}, 9));
+    }
+
+    @Test public void searchForMissingInSingletonArray() {
+        assertEquals(Optional.empty(),
+                binarySearch(new Integer[]{9}, 7));
+    }
+
+}
