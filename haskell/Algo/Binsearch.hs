0b5d031e50b7f73fed8368df364b271f71c37bad
Andrew Barchuk <andrew@raindev.io>
2017-11-13 23:03:33 +0100

    Rewrite Haskell binary search for Vector
    For Vector it actually makes sense to implement

--- haskell/Algo/Binsearch.hs
+++ haskell/Algo/Binsearch.hs
@@ -1,12 +1,15 @@
 module Algo.Binsearch (binarySearch) where
 
-binarySearch :: (Ord a) => [a] -> a -> Maybe Int
-binarySearch [] _ = Nothing
-binarySearch xs x =
-        let split  = length xs `div` 2
-            m = xs !! split
-            (lxs, hxs') = splitAt split xs
-            hxs = tail hxs'
+import Prelude hiding (null, length, take, drop)
+import Data.Vector (Vector, (!), null, length, take, drop)
+
+binarySearch :: (Ord a) => Vector a -> a -> Maybe Int
+binarySearch vec _ | null vec = Nothing
+binarySearch vec x =
+        let split  = length vec `div` 2
+            m = vec ! split
+            lvec = take split vec
+            rvec = drop (split + 1) vec
         in case compare x m of EQ -> Just split
-                               GT -> fmap (+(split + 1)) (binarySearch hxs x)
-                               LT -> binarySearch lxs x
+                               GT -> fmap (+(split + 1)) (binarySearch rvec x)
+                               LT -> binarySearch lvec x
--- haskell/Algo/BinsearchSpec.hs
+++ haskell/Algo/BinsearchSpec.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Algo.Binsearch
+import Data.Vector (empty, singleton, fromList)
 import Test.Hspec
 
 main :: IO ()
@@ -8,16 +9,16 @@ main = hspec $ do
 
   describe "Binary search" $ do
     it "finds nothing in empty list" $ do
-      binarySearch [] 9 `shouldBe` Nothing
+      binarySearch empty 9 `shouldBe` Nothing
 
     it "finds element in singleton list" $ do
-      binarySearch [3] 3 `shouldBe` Just 0
+      binarySearch (singleton 3) 3 `shouldBe` Just 0
 
     it "find element in list" $ do
-      binarySearch [1, 2, 3, 4, 5] 4 `shouldBe` Just 3
+      binarySearch (fromList [1, 2, 3, 4, 5]) 4 `shouldBe` Just 3
 
     it "finds tail element in list" $ do
-      binarySearch [2, 3, 7, 9] 9 `shouldBe` Just 3
+      binarySearch (fromList [2, 3, 7, 9]) 9 `shouldBe` Just 3
 
     it "finds head element in list" $ do
-      binarySearch [2, 3, 7, 9] 2 `shouldBe` Just 0
+      binarySearch (fromList [2, 3, 7, 9]) 2 `shouldBe` Just 0
