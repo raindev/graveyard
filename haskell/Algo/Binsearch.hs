57d0966b816c4ff53309765c545a5070d72e8358
Andrew Barchuk <andrew@raindev.io>
2017-11-13 21:06:45 +0100

    Clarify list split in Haskell binary search

--- haskell/Algo/Binsearch.hs
+++ haskell/Algo/Binsearch.hs
@@ -5,8 +5,8 @@ binarySearch [] _ = Nothing
 binarySearch xs x =
         let split  = length xs `div` 2
             m = xs !! split
-            hxs = snd $ splitAt (split + 1) xs
-            lxs = fst $ splitAt split xs
+            (lxs, hxs') = splitAt split xs
+            hxs = tail hxs'
         in case compare x m of EQ -> Just split
                                GT -> fmap (+(split + 1)) (binarySearch hxs x)
                                LT -> binarySearch lxs x
