022cb37c6c6e9a4b62b94e317ce4469490cd26e0
Andrew Barchuk <andrew@raindev.io>
2021-08-02 12:00:11 +0200

    Reduce range per elevation for rainwater
    Given that the width (distance between the first and the last point) of
    each elevation level cannot increase and is likely to decrease per
    elevation, use the start/end boundaries from each preceding level to
    reduce the range of points to look at. If the width of each elevation
    level does not decrease substantially with elevation this will actually
    lead to worse runtime due to additional computations being performed and
    larger size of the data referenced by the loop.

--- java/graveyard/src/main/java/io/raindev/rainwater/Rainwater.java
+++ java/graveyard/src/main/java/io/raindev/rainwater/Rainwater.java
@@ -4,15 +4,19 @@ class Rainwater {
     static int trap(int[] heights) {
         var result = 0;
         var elevation = 0;
+        var start = 0;
+        var end = heights.length;
         int elevationPoints;
         do {
             ++elevation;
             elevationPoints = 0;
             int leftBound = -1;
-            for (var x = 0; x < heights.length; ++x) {
+            for (var x = start; x < end; ++x) {
                 if (heights[x] >= elevation) {
                     ++elevationPoints;
-                    if (leftBound != -1) {
+                    if (leftBound == -1) {
+                        start = x;
+                    } else {
                         if (leftBound < x - 1) {
                             result += x - leftBound - 1;
                         }
@@ -20,6 +24,7 @@ class Rainwater {
                     leftBound = x;
                 }
             }
+            end = leftBound + 1;
         } while (elevationPoints > 1);
         return result;
     }
