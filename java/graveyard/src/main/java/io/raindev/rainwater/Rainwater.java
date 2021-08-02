ea53c62d1b32fa6ca6e68d972400d7360d3d98bc
Andrew Barchuk <andrew@raindev.io>
2021-08-02 13:25:02 +0200

    Rainwater: Only store points of elevation decrease
    Instead of storing duplicate values for elevation

--- java/graveyard/src/main/java/io/raindev/rainwater/Rainwater.java
+++ java/graveyard/src/main/java/io/raindev/rainwater/Rainwater.java
@@ -9,23 +9,24 @@ class Rainwater {
             return 0;
         }
         var rightBounds = new ArrayDeque<Integer>();
-        rightBounds.push(heights[heights.length - 1]);
+        rightBounds.push(heights.length - 1);
         for (int x = heights.length - 1; x >= 0; --x) {
-            if (heights[x] >= rightBounds.peek()) {
-                rightBounds.push(heights[x]);
+            if (heights[x] > heights[rightBounds.peek()]) {
+                rightBounds.push(x);
             }
         }
         var result = 0;
         var leftBound = heights[0];
-        for (var height : heights) {
-            final var groundLevel = min(leftBound, rightBounds.peek());
+        for (var x = 0; x < heights.length; ++x) {
+            final var height = heights[x];
+            final var groundLevel = min(leftBound, heights[rightBounds.peek()]);
             if (height < groundLevel) {
                 result += groundLevel - height;
             }
             if (height > leftBound) {
                 leftBound = height;
             }
-            if (height == rightBounds.peek()) {
+            if (x == rightBounds.peek()) {
                 rightBounds.pop();
             }
         }
