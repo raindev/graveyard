196aac4a4f9de09c4b4129b08b1d8734e32bcaaf
Andrew Barchuk <andrew@raindev.io>
2021-08-20 22:27:47 +0200

    Use array to store phone pad letters

--- java/graveyard/src/main/java/io/raindev/phonewords/PhoneWords.java
+++ java/graveyard/src/main/java/io/raindev/phonewords/PhoneWords.java
@@ -4,20 +4,10 @@ import java.util.List;
 import java.util.ArrayList;
 
 final class PhoneWords {
-    private static final String digitLetters(char digit) {
-        switch (digit) {
-            case '2': return "abc";
-            case '3': return "def";
-            case '4': return "ghi";
-            case '5': return "jkl";
-            case '6': return "mno";
-            case '7': return "pqrs";
-            case '8': return "tuv";
-            case '9': return "wxyz";
-            default: throw new IllegalArgumentException(
-                             String.valueOf(digit));
-        }
-    }
+    private static final String[] letters = {
+        "", "",
+        "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
+    };
 
     static List<String> combos(final String digits) {
         if (digits.isEmpty()) {
@@ -25,18 +15,17 @@ final class PhoneWords {
         }
         List<String> res = new ArrayList<>();
         var digitCount = digits.length();
-        String[] letters = new String[digitCount];
         var permutationCount = 1;
         for (int i = 0; i < digitCount; ++i) {
-            letters[i] = digitLetters(digits.charAt(i));
-            permutationCount *= letters[i].length();
+            permutationCount *= letters[digits.charAt(i) - '0'].length();
         }
         for (int i = 0; i < permutationCount; ++i) {
             var permutationNumber = i;
             var word = new StringBuilder();
             for (int digit = digitCount - 1; digit >= 0; --digit) {
-                var letterCount = letters[digit].length();
-                word.insert(0, letters[digit].charAt(permutationNumber % letterCount));
+                var digitLetters = letters[digits.charAt(digit) - '0'];
+                var letterCount = digitLetters.length();
+                word.insert(0, digitLetters.charAt(permutationNumber % letterCount));
                 permutationNumber /= letterCount;
             }
             res.add(word.toString());
