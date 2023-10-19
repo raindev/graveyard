1e202bcf525afd8731f0e5f6c5ea28b095dc9fbe
Andrew Barchuk <andrew@raindev.io>
2023-10-19 16:17:11 +0200

    Add tests for AoC 2015-1
    And use a single function

--- advent-of-code/2015/1/floors.go
+++ advent-of-code/2015/1/floors.go
@@ -1,11 +1,8 @@
-package main
+package floors
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
-func finalFloor(directions []byte) (floor int) {
+func climbFloors(directions []byte) (floor, basementStep int) {
 	for i, pointer := range directions {
 		switch pointer {
 		case '(':
@@ -15,33 +12,9 @@ func finalFloor(directions []byte) (floor int) {
 		default:
 			panic(fmt.Sprintf("unexpected character %d at %d", pointer, i))
 		}
-	}
-	return floor
-}
-
-func basementStep(directions []byte) int {
-	var floor int
-	for i, pointer := range directions {
-		switch pointer {
-		case '(':
-			floor++
-		case ')':
-			floor--
-		default:
-			panic(fmt.Sprintf("unexpected character %d at %d", pointer, i))
-		}
-		if floor == -1 {
-			return i + 1
+		if floor == -1 && basementStep == 0 {
+			basementStep = i + 1
 		}
 	}
-	return floor
-}
-
-func main() {
-	input, err := os.ReadFile("input")
-	if err != nil {
-		panic(err)
-	}
-	println("Santa will end up on floor", finalFloor(input))
-	println("Santa will go into basement after", basementStep(input), "steps")
+	return floor, basementStep
 }
---
+++ advent-of-code/2015/1/floors_test.go
@@ -0,0 +1,20 @@
+package floors
+
+import (
+	"os"
+	"testing"
+)
+
+func TestFloorClimbing(t *testing.T) {
+	input, err := os.ReadFile("input")
+	if err != nil {
+		panic(err)
+	}
+	endFloor, basementStep := climbFloors(input)
+	if endFloor != 232 {
+		t.Errorf("Santa is expected to end up on floor 232")
+	}
+	if basementStep != 1783 {
+		t.Errorf("Santa is expected to go into basement after 1783 steps")
+	}
+}
