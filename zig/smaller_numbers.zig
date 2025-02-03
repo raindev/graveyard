40abdfe418b48a0eec89b593fc21bdb80d723e30
Andrew Barchuk <andrew@raindev.io>
2025-02-03 21:58:47 +0100

    Optimize count smaller following numbers
    Implement an efficient n*log(n) solution using a treap (Zig standard
    library doesn't have a self-balancing tree)

--- zig/smaller_numbers.zig
+++ zig/smaller_numbers.zig
@@ -1,13 +1,43 @@
 const std = @import("std");
 const t = std.testing;
 
+fn reverse_order(a: anytype, b: anytype) std.math.Order {
+    if (a < b) {
+        return .gt;
+    } else if (a > b) {
+        return .lt;
+    } else if (a == b) {
+        return .eq;
+    }
+    unreachable;
+}
+const Treap = std.Treap(u8, reverse_order);
+
 // Count the number of smaller numbers to the right of each number.
 fn count_smaller_right(comptime len: usize, numbers: [len]u8) [len]u8 {
+    var treap_nodes: [len]Treap.Node = undefined;
+    var next_node: usize = 0;
+
     var counts: [len]u8 = [_]u8{0} ** len;
-    for (numbers, 0..) |n, i| for (numbers[i + 1 ..]) |right_n|
-        if (n > right_n) {
-            counts[i] += 1;
-        };
+    if (numbers.len == 0) return counts;
+    var right_numbers: Treap = .{};
+    // iterate from the end, populating the treap incrementally
+    var i = numbers.len;
+    while (i > 0) {
+        i -= 1;
+        const element = numbers[i];
+
+        var entry = right_numbers.getEntryFor(element);
+        entry.set(&treap_nodes[next_node]);
+        next_node += 1;
+
+        var iter = Treap.InorderIterator{ .current = entry.node };
+        // skip the number itself
+        if (iter.next() == null) continue;
+        var count: u8 = 0;
+        while (iter.next()) |_| : (count += 1) {}
+        counts[i] = count;
+    }
     return counts;
 }
 
