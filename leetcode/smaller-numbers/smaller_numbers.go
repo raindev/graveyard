2d3fdbcf8b4a7dd4237cd151a44c2c63ce5471dd
Andrew Barchuk <andrew@raindev.io>
2025-02-05 00:23:43 +0100

    Add an optimal linear solution for smaller numbers

--- leetcode/smaller-numbers/smaller_numbers.go
+++ leetcode/smaller-numbers/smaller_numbers.go
@@ -1,16 +1,21 @@
 package smallerafterself
 
 func smallerNumbersThanCurrent(nums []int) []int {
+	counts := make([]int, 101)
+	// count each number first
+	for _, n := range nums {
+		counts[n]++
+	}
+	// tally up counts in ascending order
+	total_count := 0
+	for i := range counts {
+		curr := counts[i]
+		counts[i] = total_count
+		total_count += curr
+	}
 	res := make([]int, len(nums))
-	for i, x := range nums {
-		for j, y := range nums {
-			if i == j {
-				continue
-			}
-			if y < x {
-				res[i]++
-			}
-		}
+	for i, n := range nums {
+		res[i] = counts[n]
 	}
 	return res
 }
