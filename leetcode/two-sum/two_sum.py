033975cefe158cbfb0e1fbca5aebc2310012658f
Andrew Barchuk <andrew@raindev.io>
2025-03-02 10:04:56 +0100

    Simplify Two Sum Python solution to one iteration
    Check for an existing complement as the index dictionary is constructed,
    generalizing the special case for complementary being the same number

--- leetcode/two-sum/two_sum.py
+++ leetcode/two-sum/two_sum.py
@@ -2,24 +2,18 @@ class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
         """Return the indexes of two elements of num that sum up to target.
 
-        >>> Solution().twoSum([1, 5, 7, 4], 8)
-        [2, 0]
+        >>> set(Solution().twoSum([1, 5, 7, 4], 8))
+        {0, 2}
 
-        >>> Solution().twoSum([2, 5, 7, 2], 4)
-        [0, 3]
+        >>> set(Solution().twoSum([2, 5, 7, 2], 4))
+        {0, 3}
         """
         indexes = {}
         for i, n in enumerate(nums):
-            if n in indexes:
-                if n + n == target:
-                    return [indexes[n], i]
-                else:
-                    continue
+            complement = target - n
+            if complement in indexes:
+                return [indexes[complement], i]
             indexes[n] = i
-        for i, n in enumerate(nums):
-            right = target - n
-            if right in indexes:
-                return [indexes[right], i]
         return []
 
 if __name__ == "__main__":
