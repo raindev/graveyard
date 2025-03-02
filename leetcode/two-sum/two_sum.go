539bc5ad97814167a28b58fd6d411e5abb217374
Andrew Barchuk <andrew@raindev.io>
2025-03-02 10:48:41 +0100

    Implement a one pass Go solution to Two Sum

--- leetcode/two-sum/two_sum.go
+++ leetcode/two-sum/two_sum.go
@@ -1,17 +1,12 @@
 package twosum
 
-// The solution works correctly when the complements are equal. Even though the
-// index of one of the complements is overwritten in nums_indexes, it will be
-// available when iterated over in the second pass.
 func twoSum(nums []int, target int) []int {
-	nums_indexes := make(map[int]int)
+	indexes := make(map[int]int)
 	for i, n := range nums {
-		nums_indexes[n] = i
-	}
-	for l := range nums {
-		if r, ok := nums_indexes[target-nums[l]]; ok && r != l {
-			return []int{l, r}
+		if complement_idx, ok := indexes[target-n]; ok {
+			return []int{complement_idx, i}
 		}
+		indexes[n] = i
 	}
 	return nil
 }
