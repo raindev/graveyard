325bd5ad337f73ce9770aeac34c469d82d77c214
Andrew Barchuk <andrew@raindev.io>
2025-03-09 11:03:16 +0100

    Make richest customer work for negative wealth

--- leetcode/1672-richest-customer-wealth/richest_customer.go
+++ leetcode/1672-richest-customer-wealth/richest_customer.go
@@ -1,15 +1,19 @@
 package richestcustomerwealth
 
 func maximumWealth(accounts [][]int) int {
-	maxWealth := 0
-	for _, customerAccounts := range accounts {
-		customerWealth := 0
-		for _, balance := range customerAccounts {
-			customerWealth += balance
-		}
+	maxWealth := sum(accounts[0])
+	for _, customerAccounts := range accounts[1:] {
+		customerWealth := sum(customerAccounts)
 		if customerWealth > maxWealth {
 			maxWealth = customerWealth
 		}
 	}
 	return maxWealth
+}
+
+func sum(numbers []int) (sum int) {
+	for _, n := range numbers {
+		sum += n
+	}
+	return
 }
