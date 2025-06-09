1f20f49c6c73956bd877e2e7f538d652400c015a
Andrew Barchuk <andrew@raindev.io>
2025-06-09 18:05:24 +0200

    More K&R chapter 1 code

---
+++ K&R/1/digit-histogram.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+main()
+{
+	int c, i, j;
+	int ndigit[10];
+
+	for (i = 0; i < 10; ++i)
+		ndigit[i] = 0;
+
+	while ((c = getchar()) != EOF)
+		if (c >= '0' && c <= '9')
+			++ndigit[c-'0'];
+
+	for (i = 0; i < 10; ++i) {
+		printf("%d ", i);
+		for (j = 0; j < ndigit[i]; ++j)
+			putchar('*');
+		putchar('\n');
+	}
+}
