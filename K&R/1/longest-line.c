53f4351206b460af87e87ba0ed8adf2d8cc30862
Andrew Barchuk <andrew@raindev.io>
2025-06-15 16:30:41 +0200

    K&R C: allow longest line to exceed buffer size

--- K&R/1/longest-line.c
+++ K&R/1/longest-line.c
@@ -7,18 +7,32 @@ void copy(char to[], char from[]);
 
 main()
 {
-	int len;
-	int max;
-	char line[MAXLINE];
+	int llen, clen, mlen;
+	int is_prefix;
+	char chunk[MAXLINE];
+	char prefix[MAXLINE];
 	char longest[MAXLINE];
 
-	max = 0;
-	while ((len = _getline(line, MAXLINE)) > 0)
-		if (len > max) {
-			max = len;
-			copy(longest, line);
+	mlen = llen = 0;
+	is_prefix = 1;
+	while ((clen = _getline(chunk, MAXLINE)) > 0) {
+		if (llen + clen > mlen) {
+			if (is_prefix)
+				copy(longest, chunk);
+			else
+				copy(longest, prefix);
+			mlen = llen + clen;
+		} else if (is_prefix && clen == MAXLINE - 1 && chunk[clen - 1] != '\n')
+			copy(prefix, chunk);
+		if (chunk[clen - 1] == '\n') {
+			llen = 0;
+			is_prefix = 1;
+		} else {
+			llen = llen + clen;
+			is_prefix = 0;
 		}
-	if (max > 0)
+	}
+	if (mlen > 0)
 		printf("%s", longest);
 	return 0;
 }
