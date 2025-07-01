5534d276d3d02ee11808d92864a3fd89e7bed71d
Andrew Barchuk <andrew.barchuk@zalando.de>
2025-07-01 22:49:46 +0200

    K&R C 2-1

---
+++ K&R/1/limits.c
@@ -0,0 +1,33 @@
+#include <assert.h>
+#include <float.h>
+#include <stdio.h>
+#include <limits.h>
+
+main()
+{
+	// two's complement negative number representation
+	assert(~0 == -1);
+
+	char c = ~0;
+	if (c < 0)
+		printf("char is signed\n");
+	else
+		printf("char is unsigned\n");
+
+	printf("char: %d - %d\n", CHAR_MIN, CHAR_MAX);
+	printf("signed char: %d - %d\n", SCHAR_MIN, SCHAR_MAX);
+	printf("unsigned char: %d - %d\n", 0, UCHAR_MAX);
+
+	printf("signed short: %d - %d\n", SHRT_MIN, SHRT_MAX);
+	printf("unsigned short: %d - %d\n", 0, USHRT_MAX);
+
+	printf("signed int: %d - %d\n", INT_MIN, INT_MAX);
+	printf("unsigned int: %u - %u\n", 0, UINT_MAX);
+
+	printf("signed long: %ld - %ld\n", LONG_MIN, LONG_MAX);
+	printf("unsigned long: %d - %lu\n", 0, ULONG_MAX);
+
+	printf("float: %f - %f\n", FLT_MIN, FLT_MAX);
+	printf("double: %f - %f\n", DBL_MIN, DBL_MAX);
+	printf("long double: %Lf - %Lf\n", LDBL_MIN, LDBL_MAX);
+}
