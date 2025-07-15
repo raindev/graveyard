14b77eb3f1da8cbbe7a9197dac2ed854050d62b6
Andrew Barchuk <andrew@raindev.io>
2025-07-15 20:24:46 +0200

    Time two implementations for K&R C 3-1

--- K&R/3/binsearch.c
+++ K&R/3/binsearch.c
@@ -1,30 +1,50 @@
 #include <assert.h>
 #include <stdio.h>
+#include <time.h>
 
 #define INPUT_SIZE 1000000
+#define ITERATIONS 10
 
 int binsearch(int x, int v[], int n);
 int _binsearch(int x, int v[], int n);
 
 main()
 {
-	int sorted[INPUT_SIZE];
 	int i;
-
-	for (i = 0; i < INPUT_SIZE; ++i)
-		sorted[i] = i;
-	const int target = 257233;
-
-	// TODO time the calls
-
-	assert(binsearch(target, sorted, INPUT_SIZE) == 257233);
-	assert(_binsearch(target, sorted, INPUT_SIZE) == 257233);
-
 	int input[] = {0, 2, 4, 6, 8};
+
 	for (i = 0; i < 5; ++i) {
 		assert(_binsearch(2 * i, input, 5) == i);
 		assert(_binsearch(2 * i + 1, input, 5) == -1);
 	}
+
+	int sorted[INPUT_SIZE];
+
+	for (i = 0; i < INPUT_SIZE; ++i)
+		sorted[i] = i;
+
+	struct timespec start, end;
+	int total_nsec, target;
+
+	total_nsec = 0;
+	for (i = 0; i < ITERATIONS; ++i) {
+		target = 257233 + i * 100;
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+		assert(binsearch(target, sorted, INPUT_SIZE) == target);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+		total_nsec += end.tv_nsec - start.tv_nsec;
+	}
+	printf("%dns/iteration\n", total_nsec / ITERATIONS);
+
+	total_nsec = 0;
+	for (i = 0; i < ITERATIONS; ++i) {
+		target = 257233 + i * 100;
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+		assert(_binsearch(target, sorted, INPUT_SIZE) == target);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+		total_nsec += end.tv_nsec - start.tv_nsec;
+	}
+	printf("%dns/iteration\n", total_nsec / ITERATIONS);
 }
 
 int _binsearch(int x, int v[], int n)
