a3f403bde257ad0fcfe742a01df4cbcc33703f46
Andrew Barchuk <andrew@raindev.io>
2026-06-17 10:40:24 +0200

    LC70 star climbing: a dynamic solution

--- leetcode/70-climbing-stairs/stairs.go
+++ leetcode/70-climbing-stairs/stairs.go
@@ -4,8 +4,12 @@ func climbStairs(n int) int {
 	if n == 1 {
 		return 1
 	}
-	if n == 2 {
-		return 2
+
+	res := make([]int, n)
+	res[0] = 1
+	res[1] = 2
+	for i := 2; i < n; i++ {
+		res[i] = res[i-1] + res[i-2]
 	}
-	return climbStairs(n-2) + climbStairs(n-1)
+	return res[n-1]
 }
